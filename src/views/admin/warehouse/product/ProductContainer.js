b63d7166fdb2c858043474426cb1a8f2eeb48adb
Mohammad <attarzoghi.m1@gmail.com>
2023-12-01 01:07:21 +0330

    10

--- src/views/admin/warehouse/product/ProductContainer.js
+++ src/views/admin/warehouse/product/ProductContainer.js
@@ -1,42 +1,44 @@
 import React from 'react'
-
+import './ProductContainer.css'
 export const ProductContainer = () => {
   return (
     <>
       <h2>Products</h2>
-      <div>
-        <h3>Product Info:</h3>
-        <div>
-          <label>Code: </label>
-          <input />
+      <div className='product'>
+        <div className='Product-info'>
+          <h3>Product Info:</h3>
+          <div>
+            <label>Code: </label>
+            <input />
+          </div>
+          <div>
+            <lable>Title: </lable>
+            <input />
+          </div>
+          <div>
+            <lable>Price: </lable>
+            <input />
+          </div>
+          <div>
+            <button>Save</button>
+            <button>Clear</button>
+          </div>
         </div>
-        <div>
-          <lable>Title: </lable>
-          <input />
-        </div>
-        <div>
-          <lable>Price: </lable>
-          <input />
-        </div>
-        <div>
-          <button>Save</button>
-          <button>Clear</button>
-        </div>
-      </div>
-      <div>
-        <h3>Product List</h3>
-        <div>
-          Code:
-        </div>
-        <div>
-          Price:
-        </div>
-        <div>
-          Title:
-        </div>
-        <div>
-          <button>Edit</button>
-          <button>Remove</button>
+        <div className='product-list'>
+          <h3>Product List</h3>
+          <div>
+            Code:
+          </div>
+          <div>
+            Price:
+          </div>
+          <div>
+            Title:
+          </div>
+          <div>
+            <button>Edit</button>
+            <button>Remove</button>
+          </div>
         </div>
       </div>
       <div>ProductContainer</div>
