82fa729b386a2f632593d80be2f5d335b64c6feb
Mohammad <attarzoghi.m1@gmail.com>
2023-12-01 21:45:10 +0330

    2

--- src/views/admin/warehouse/product/ProductContainer.js
+++ src/views/admin/warehouse/product/ProductContainer.js
@@ -1,39 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ProductContainer.css'
+
 export const ProductContainer = () => {
+
+  const [products, setProduct] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(-1);
+ // const [first, setfirst] = useState(second)
+
+  const save = () => {
+    let product = {};
+    product.code = document.getElementById("code").value;
+    product.price = document.getElementById("price").value;
+    product.title = document.getElementById("title").value;
+    alert(product.title);
+    setProduct([...products,product]);
+  }
+
+
   return (
     <>
       <h2>Products</h2>
-      <div className='product'>
-        <div className='Product-info'>
+      <div className='product-container'>
+        <div className='product-info'>
           <h3>Product Info:</h3>
           <div>
             <label>Code: </label>
-            <input />
+            <input id="code" />
           </div>
           <div>
             <lable>Title: </lable>
-            <input />
+            <input id="title" />
           </div>
           <div>
             <lable>Price: </lable>
-            <input />
+            <input id="price" />
           </div>
           <div>
-            <button>Save</button>
+            <button onClick={save}>Save</button>
             <button>Clear</button>
           </div>
         </div>
         <div className='product-list'>
-          <h3>Product List</h3>
+          <h3>Product List: </h3>
           <div>
-            Code:
-          </div>
-          <div>
-            Price:
-          </div>
-          <div>
-            Title:
+            <div>
+              Code: 
+            </div>
+            <div>
+              Price:
+            </div>
+            <div>
+              Title:
+            </div>
           </div>
           <div>
             <button>Edit</button>
