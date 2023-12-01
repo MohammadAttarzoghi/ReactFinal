cf2e8a6433d9ce229da8c640bfdf5fa69a9bbbcf
Mohammad <attarzoghi.m1@gmail.com>
2023-12-01 22:57:01 +0330

    5

--- src/views/admin/warehouse/product/ProductContainer.js
+++ src/views/admin/warehouse/product/ProductContainer.js
@@ -5,15 +5,15 @@ export const ProductContainer = () => {
 
   const [products, setProduct] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(-1);
- // const [first, setfirst] = useState(second)
+  // const [first, setfirst] = useState(second)
 
   const save = () => {
     let product = {};
     product.code = document.getElementById("code").value;
     product.price = document.getElementById("price").value;
     product.title = document.getElementById("title").value;
-    alert(product.title);
-    setProduct([...products,product]);
+  //  alert(product.title);
+    setProduct([...products, product]);
   }
 
 
@@ -42,21 +42,26 @@ export const ProductContainer = () => {
         </div>
         <div className='product-list'>
           <h3>Product List: </h3>
-          <div>
+
+          {products.map((item,index) => (
             <div>
-              Code: 
+              <h4>item index: {index+1}</h4>
+              <div>
+                Code: {item.code}
+              </div>
+              <div>
+                Price:{item.price}
+              </div>
+              <div>
+                Title:{item.Title}
+              </div>
+
+              <div>
+                <button>Edit</button>
+                <button>Remove</button>
+              </div>
             </div>
-            <div>
-              Price:
-            </div>
-            <div>
-              Title:
-            </div>
-          </div>
-          <div>
-            <button>Edit</button>
-            <button>Remove</button>
-          </div>
+          ))}
         </div>
       </div>
       <div>ProductContainer</div>
